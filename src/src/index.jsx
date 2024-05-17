96fe8eab256f85ecdb424207e7cd1a268c433ceb
FGuerreir0 <ffsguerreiro@hotmail.com>
2024-05-17 18:12:33 +0000

    -

--- src/src/index.jsx
+++ src/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Routes, Route } from "react-router-dom";
 import App from './components/App/App.jsx'
-import Nomatch from "./components/Nomatch/Nomatch.jsx";
+//import Nomatch from "./components/Nomatch/Nomatch.jsx";
 import { BrowserRouter } from "react-router-dom";
 import './index.css'
 
@@ -13,8 +13,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/" element={<App />}/>
         {/*<Route path="/politica-de-qualidade" element={<Qualitypolicy />} />
         <Route path="/our-solutions" element={<ServicesPage />} />
-        <Route path="/carreiras" element={<JobPage />} />*/}
-        <Route path="/*" element={<Nomatch />} />
+        <Route path="/carreiras" element={<JobPage />} />
+        <Route path="/*" element={<Nomatch />} />*/}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
